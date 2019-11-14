5570340e2a03843f4796639e05510913e72f21b3
tarabore <tarabore@gmail.com>
2019-11-14 20:35:47 -0300

    Esta es la version inicial

---
+++ src/himno/Himno.java
@@ -0,0 +1,12 @@
+package himno;
+
+public class Himno {
+
+     public static void main(String[] args) {
+         System.out.println("Oid mortales el grito sagrado");
+         System.out.println("Libertad, Exclavitud y Miseria");
+         System.out.println("Y los libres del mundo responden");
+         System.out.println("A pagar la deuda!");
+    }
+    
+}
