133b75f4c65863f9a461b71e310be62158085672
tarabore <tarabore@gmail.com>
2021-11-29 21:17:23 -0300

    Modifico la frase pueblo salud por salud y bienestar

--- src/himno/Himno.java
+++ src/himno/Himno.java
@@ -6,7 +6,7 @@ public class Himno {
          System.out.println("Oid mortales el grito sagrado");        
          System.out.println("Y los libres del mundo responden");
          System.out.println("Otra estrofa del himno");
-         System.out.println("Al gran pueblo Argentino salud");
+         System.out.println("Al gran pueblo Argentino salud y bienestar");
          System.out.println("Coronados de gloria vivaaaaaaaaaamooooossss");
     }
     
