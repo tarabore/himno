76fd730be306fd3008e3f64d95ada7357c098a8d
tarabore <tarabore@gmail.com>
2025-11-11 18:52:57 -0300

    Merge origin/master
    # Conflicts:
    #	src/calcular/Calcular.java

--- src/himno/Himno.java
+++ src/himno/Himno.java
@@ -8,7 +8,7 @@ public class Himno {
          System.out.println("Otra estrofa del himno");
          System.out.println("Al gran pueblo Argentino salud y buen fin de semana");
          System.out.println("Coronados de gloria vivaaaaaaaaaamooooossss");
-         System.out.println("O juremos con gloria...");
+         System.out.println("O juremos con gloria moriirrrr");
     }
     
 }
