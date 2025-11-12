2fb053e0ac1b8b9b6f820bf8a968541d3fab24e5
tarabore <tarabore@gmail.com>
2025-11-12 17:16:26 -0300

    Merge origin/master
    # Conflicts:
    #	src/calcular/Calcular.java

--- src/himno/Himno.java
+++ src/himno/Himno.java
@@ -7,8 +7,7 @@ public class Himno {
          System.out.println("Y los libres del mundo responden");
          System.out.println("Otra estrofa del himno");
          System.out.println("Al gran pueblo Argentino salud y buen fin de semana");
-         System.out.println("Coronados de gloria vivaaaaaaaaaamooooossss");
-         System.out.println("O juremos con gloria moriirrrr");
-    }
+         System.out.println("Una línea antes del estribillo");
+         System.out.println("Coronados de gloria vivaaaaaaaaaamooooossss");    }
     
 }
